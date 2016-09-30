ea999aead497921662634c9cd2a9d78bf69ad6f7
centos <jian@localhost.localdomain>
2016-09-29 22:14:45 -0700

    first edit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+class NestedInteger {
+public:
+  bool isInteger() const {
+    
+  }
+
+  int getInteger() const {
+    
+  }
+
+  const vector<NestInteger> & getList() const {
+    
+  }
+
+private:
+  int myInt;
+  vector<NestInteger> myVec;
+};
+
+class solution {
+public:
+  int depthSum() {
+    
+  }
+};
